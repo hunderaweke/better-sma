e21a5b21da13523a5f1e81bdd36ebd2e1792d027
Hundera Awoke <hunderaweke@gmail.com>
2026-01-20 21:58:31 +0300

    initial: start the basic layout

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <section className="flex w-full justify-center items-center h-screen">
+      <h1 className="text-5xl">Send Anon Messages Better</h1>
+    </section>
+  );
+}
+
+export default Home;
